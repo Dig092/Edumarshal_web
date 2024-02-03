019ed599854607773a9afaa0a5c6bafa0ecbc1a1
Vedantdevam <vedantdevamsrivatava@gmail.com>
2024-02-04 01:26:28 +0530

    Completed Login Page

--- Student_Side/src/App.jsx
+++ Student_Side/src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage";
 import DashBoardPage from "./pages/DashBoardPage";
 import ProfilePage from "./pages/ProfilePage";
 import Classroom from "./pages/Classroom";
+import LoginPage from "./pages/LoginPage";
+// import HostelPage from "./pages/HostelPage";
 
 export default function App() {
     return (
@@ -12,6 +14,8 @@ export default function App() {
                 <Route exact path="/" element={<HomePage />} />
                 <Route exact path="/dashboard" element={<DashBoardPage />} />
                 <Route exact path="/profile" element={<ProfilePage/>}/>
+                <Route exact path="/login" element={<LoginPage />} />
+                {/* <Route exact path="/hostelpage" element={<HostelPage />} /> */}
                 <Route exact path="/Classroom" element={<Classroom/>}/>
             </Routes>
         </Router>
