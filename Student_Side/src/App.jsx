45592fe1d3f8075cb842be1c1901b29a1e298dc3
Dhruval Gupta <diggupta092@gmail.com>
2024-02-04 15:33:36 +0530

    update Login Directory

--- Student_Side/src/App.jsx
+++ Student_Side/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import DashBoardPage from "./pages/DashBoardPage";
 import ProfilePage from "./pages/ProfilePage";
 import Classroom from "./pages/Classroom";
@@ -11,10 +10,9 @@ export default function App() {
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={<HomePage />} />
+                <Route exact path="/" element={<LoginPage />} />
                 <Route exact path="/dashboard" element={<DashBoardPage />} />
                 <Route exact path="/profile" element={<ProfilePage/>}/>
-                <Route exact path="/login" element={<LoginPage />} />
                 {/* <Route exact path="/hostelpage" element={<HostelPage />} /> */}
                 <Route exact path="/Classroom" element={<Classroom/>}/>
             </Routes>
