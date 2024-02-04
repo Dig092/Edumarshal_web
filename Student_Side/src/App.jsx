72229e88d3ce79e0fa8f41736b665145365fe95d
abhinav2210103 <abhinav2210103@akgec.ac.in>
2024-02-05 01:56:48 +0530

    Fees navbar Created

--- Student_Side/src/App.jsx
+++ Student_Side/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import DashBoardPage from "./pages/DashBoardPage";
 import ProfilePage from "./pages/ProfilePage";
 import Classroom from "./pages/Classroom";
+import Fees from "./pages/Fees";
 
 export default function App() {
     return (
@@ -13,6 +14,7 @@ export default function App() {
                 <Route exact path="/dashboard" element={<DashBoardPage />} />
                 <Route exact path="/profile" element={<ProfilePage/>}/>
                 <Route exact path="/Classroom" element={<Classroom/>}/>
+                <Route exact path="/Fees" element={<Fees/>}/>
             </Routes>
         </Router>
     );
