15f249b547aefc643dbb6f7d1f06eae255032f71
Akshar Saxena <aksharsaxena2003@gmail.com>
2024-02-15 12:49:02 +0530

    added assignment, timetable API, loader

--- Student_Side/src/components/DashboardComponents/SideCard.jsx
+++ Student_Side/src/components/DashboardComponents/SideCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import SideSubCard from "./SideSubCard";
 
-export default function SideCard() {
+export default function SideCard(props) {
     const [open, setOpen] = useState(false);
     const [height, setHeight] = useState("100px");
     const openHandler = () => {
@@ -16,7 +17,9 @@ export default function SideCard() {
         >
             {" "}
             <div className="flex max-[500px]:translate-x-[5%] w-[90%] m-auto mt-2 justify-between">
-                <h1 className="font-semibold text-[15px]">Mathematics</h1>
+                <h1 className="font-semibold text-[15px]">
+                    {props.array.subject}
+                </h1>
                 <img
                     onClick={openHandler}
                     src={`${open ? "./icons/up.png" : "./icons/down.png"}`}
@@ -32,7 +35,7 @@ export default function SideCard() {
                         alt=""
                     />
                     <div className="flex flex-col justify-center">
-                        <h1 className="text-[12px]">Minakshi Mam</h1>
+                        <h1 className="text-[12px]">{props.array.teacher}</h1>
                         <h2 className="text-[9px] text-gray-500">
                             Assitant Professor
                         </h2>
@@ -41,48 +44,9 @@ export default function SideCard() {
                 <span className="text-[9px] text-gray-600">view all</span>
             </div>
             <div>
-                <div
-                    className="flex max-[500px]:translate-x-[5%] justify-between items-center w-[90%] m-auto my-3 mt-5 bg-[#F2F6FF] py-1 px-2 rounded-lg"
-                    style={{ display: `${open ? "flex" : "none"}` }}
-                >
-                    <div className="flex  items-center">
-                        <img
-                            className="w-[30px] h-[30px] mr-2"
-                            src="./icons/task.png"
-                            alt=""
-                        />
-                        <div>
-                            <h1 className="text-[10px] font-medium">
-                                Statical Technique I
-                            </h1>
-                            <h2 className="text-[8px] text-gray-500">
-                                Status - <span>Pending</span>
-                            </h2>
-                        </div>
-                    </div>
-                    <h3 className="text-[9px]">4:30pm</h3>
-                </div>
-                <div
-                    className="flex max-[500px]:translate-x-[5%] justify-between items-center w-[90%] m-auto my-3 bg-[#F2F6FF] py-1 px-2 rounded-lg"
-                    style={{ display: `${open ? "flex" : "none"}` }}
-                >
-                    <div className="flex items-center">
-                        <img
-                            className="w-[30px] h-[30px] mr-2"
-                            src="./icons/task.png"
-                            alt=""
-                        />
-                        <div>
-                            <h1 className="text-[10px] font-medium">
-                                Statical Technique I
-                            </h1>
-                            <h2 className="text-[8px] text-gray-500">
-                                Status - <span>Pending</span>
-                            </h2>
-                        </div>
-                    </div>
-                    <h3 className="text-[9px]">4:30pm</h3>
-                </div>
+                {props.array.assignment?.map((assignment, id) => (
+                    <SideSubCard key={id} open={open} desc={assignment} />
+                ))}
             </div>
         </div>
     );
--- Student_Side/src/components/ProfileComponents/ProfileSection.jsx
+++ Student_Side/src/components/ProfileComponents/ProfileSection.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import profileSectionMenu from "../../constants/profileSectionInput.json";
 
 export default function ProfileSection() {
     const [activeoption, setActiveoption] = useState(0);
@@ -8,7 +10,7 @@ export default function ProfileSection() {
     return (
         <div className="bg-[#ffffff] h-[72vh] rounded-3xl mx-4 mt-4 overflow-y-auto">
             {/*select option profile*/}
-            <div className="h-[9.6vh] sticky top-0 bg-white mr-4  flex flex-wrap items-center gap-3 text-lg ml-6">
+            <div className="h-[9.6vh] z-50 sticky top-0 bg-white mr-4  flex flex-wrap items-center gap-3 text-base l-6">
                 {[
                     "Personal Info",
                     "Contact Details",
@@ -24,7 +26,7 @@ export default function ProfileSection() {
                         className={`${
                             activeoption === index ? "border-b-4" : "border-b-0"
                         }
-                     flex items-center p-1 font-medium gap-2 cursor-pointer ml-8 border-[#004BB8]`}
+                        flex items-center p-1 font-medium gap-2 cursor-pointer ml-8 border-[#004BB8]`}
                     >
                         <span>{item}</span>
                     </div>
@@ -34,9 +36,28 @@ export default function ProfileSection() {
             {activeoption === 0 && (
                 //  Input Box
                 // <InputBox />
-                <div className="w-[95%] m-auto flex h-full justify-evenly">
-                    <div className="w-[25%] bg-[#F2F6FF] h-full"></div>
-                    <div className="w-[70%] bg-[#F2F6FF] h-full"></div>
+                <div className="w-[95%] pb-10 m-auto flex h-fit justify-evenly">
+                    <div className="w-[25%] pb-6 items-center rounded-xl flex flex-col bg-[#F2F6FF] h-full">
+                        <img
+                            src="./displayPicture.png"
+                            className="w-[80%] mt-6"
+                            alt=""
+                        />
+                        {profileSectionMenu?.map((element, id) => (
+                            <TextField
+                                style={{
+                                    margin: `${
+                                        id == 0 ? "30px 0 10px 0" : "10px 0"
+                                    }`,
+                                }}
+                                variant="outlined"
+                                label={element.label}
+                                type={element.type}
+                                placeholder={element.placeholder}
+                            />
+                        ))}
+                    </div>
+                    <div className="w-[70%] rounded-xl bg-[#F2F6FF] h-full"></div>
                 </div>
                 // Input Box Ends
             )}
