ae6325e6e09c74c7be5a04791d18b1147260cbe4
abhineet <abhii.neat@gmail.com>
2024-02-17 04:35:00 +0530

    made classnotes card

--- Student_Side/src/components/ClassroomComponents/Classnotes.jsx
+++ Student_Side/src/components/ClassroomComponents/Classnotes.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ClassnotesCard from './ClassnotesCard'
 
 const Classnotes = () => {
+  
   return (
-    <div>Classnotes</div>
+    <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-y-auto">
+    <div className="h-[9.6vh] sticky top-0 bg-white mr-4  flex flex-wrap items-center gap-3 text-lg ml-6">
+        <div className='flex items-center p-3 font-medium gap-2 cursor-pointer ml-8 mt-5'>
+          All Class Notes
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+                    <div className="w-[94%]  h-[1.5px] bg-[#D9D9D9] my-0"></div>
+        </div>
+      <ClassnotesCard/>
+
+
+
+
+
+
+    </div>
   )
 }
 
---
+++ Student_Side/src/components/ClassroomComponents/ClassnotesCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import profile from '../../assets/classNotesProfile.svg'
+import folder from '../../assets/classNotesFolder.svg'
+function ClassnotesCard() {
+  return (
+    <div className="m-8 flex flex-col justify-center w-[18rem] rounded-[15px] overflow-hidden shadow-lg border border-[#7F7F7F]">
+        <div className='bg-[#004BB8]  '>
+        <div className='flex w-2/3 font-medium text-2xl text-white ml-7 mt-4'>Data Structure</div>
+        <div className='ml-7 font-light text-xs text-white'>2023-24</div>
+        <div className='flex justify-between'>
+        <div className='ml-7 mt-2 mb-0 font-normal text-base text-white'>Swati Tomar </div>
+        <div className='rounded-[15px] border px-5 text-2xl bg-[#D3E5FE] text-white mr-2 mb-1 py-0'>S</div>
+        </div>
+        </div>
+        <div className='py-12'></div>
+        <div className="w-[100%]  h-[1.5px] bg-[#D9D9D9] my-0"></div>
+        <div className='flex'>
+        <img src={profile} className='py-2 ml-[70%] cursor-pointer' alt="" />
+        <img src={folder} className='py-2 ml-5 cursor-pointer ' alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default ClassnotesCard
