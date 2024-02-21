29a46f828bb721775e5302323e7e87815d9544c7
abhineet <abhii.neat@gmail.com>
2024-02-22 03:50:09 +0530

    Created exam card

---
+++ Student_Side/src/components/ClassroomComponents/ExamComponent.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Examlogo from '../../assets/ExamComponent.svg'
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/system';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 10,
+    borderRadius: 50,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      borderRadius: 5,
+      backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+    },
+  }));
+function ExamComponent() {
+  
+  return (
+    <div className="m-8 flex flex-col justify-center w-[25rem] rounded-[15px] overflow-hidden shadow-lg  bg-[#F2F6FF] border border-[#C4C4C4]">
+        <div className='flex ml-4 mt-3'>
+        <img src={Examlogo} className='w-1/6' alt="" />
+      <div className='flex flex-col mt-1'>
+        <span className='text-[#004BB8] font-medium text-lg ml-2 mt-3 '>Data Structure</span>
+        <span className='text-[#004BB8] font-medium text-xs ml-2 mt-2'>By Swati Tomar</span>
+      </div>
+      <div className='mt-[3rem]'>
+      <span className='text-[#004BB8] font-medium text-4xl mt-[5rem] ml-2'>45/50</span>
+      <span className='text-[#004BB8] font-medium text-xs '>Marks</span>
+      </div>
+      </div>
+      <span className='text-[#004BB8] font-medium text-xs ml-5 mt-1'>Your percentage</span>
+      <LinearProgress variant="determinate" value={75} sx={{height: 10, borderRadius:5, backgroundColor:'#FFFFFF', '& .MuiLinearProgress-bar': {backgroundColor: '#004BB8', borderRadius:5},}} className='mb-5 w-[22rem] ml-5 mt-1'/>
+
+    </div>
+  )
+}
+
+export default ExamComponent
--- Student_Side/src/components/ClassroomComponents/Exams.jsx
+++ Student_Side/src/components/ClassroomComponents/Exams.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ExamComponent from './ExamComponent';
 
 const Exams = () => {
+  const [activeoption, setActiveoption] = useState(0);
+    const handleoptionClick = (index) => {
+        setActiveoption(index);
+    };
   return (
-    <div>Exams</div>
+    <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-y-auto">
+        <div className="h-[9.6vh] sticky top-0 bg-white mr-4  flex flex-wrap items-center gap-3 text-lg ml-6">
+                {[
+                    "Your Result",
+                    "Recent Assignment",
+                ].map((item, index) => (
+                    <div
+                        key={index}
+                        onClick={() => handleoptionClick(index)}
+                        className={`${
+                            activeoption === index ? "border-b-4" : "border-b-0"
+                        }
+                     flex items-center p-3 font-medium gap-2 cursor-pointer ml-8 mt-5  border-[#004BB8]`}
+                    >
+                        <span>{item}</span>
+                    </div>
+                ))}
+            </div>
+            <div className="flex justify-center items-center">
+                    <div className="w-[94%]  h-[1.5px] bg-[#D9D9D9] my-0"></div>
+            </div>
+            <ExamComponent/>
+            </div>
+
   )
 }
 
