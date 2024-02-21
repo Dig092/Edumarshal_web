539bcac377bcdd827c2384eb2e7b0411d02f7cd5
abhineet <abhii.neat@gmail.com>
2024-02-22 03:51:20 +0530

    changes in exam component

--- Student_Side/src/components/ClassroomComponents/ExamComponent.jsx
+++ Student_Side/src/components/ClassroomComponents/ExamComponent.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import Examlogo from '../../assets/ExamComponent.svg'
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
-import { styled } from '@mui/system';
 
-const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-    height: 10,
-    borderRadius: 50,
-    [`&.${linearProgressClasses.colorPrimary}`]: {
-      backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
-    },
-    [`& .${linearProgressClasses.bar}`]: {
-      borderRadius: 5,
-      backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
-    },
-  }));
 function ExamComponent() {
   
   return (
