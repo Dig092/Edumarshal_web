2b44abefa4c24138d0c83af2216fb996eedcecc1
abhineet <abhii.neat@gmail.com>
2024-02-27 15:18:54 +0530

    changes in class notes

--- Student_Side/src/components/ClassroomComponents/Classnotes.jsx
+++ Student_Side/src/components/ClassroomComponents/Classnotes.jsx
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+// Classnotes component
+import React, { useEffect, useState } from 'react';
 import ClassnotesCard from './ClassnotesCard';
 import ClassnotesTable from './ClassnotesTable';
+import axios from 'axios';
 
 const Classnotes = () => {
   const [pageName, setPageName] = useState("All Class Notes");
+  const [classNotes, setClassNotes] = useState([]);
+  const [subjectName, setSubjectName] = useState("");
+
+  useEffect(() => {
+    getClassnotes();
+  }, []); 
+
+  const getClassnotes = () => {
+    axios
+      .get("https://akgec-edu.onrender.com/v1/student/classNotes", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        const responseData = res.data.classNotes;
+        console.log(responseData);
+        setClassNotes(responseData);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   return (
-    <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-y-auto">
+    <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-y-auto overflow-x-hidden">
       <div className="h-[9.6vh] sticky top-0 bg-white mr-4  flex flex-wrap items-center gap-3 text-lg ml-6">
         <div className='flex items-center p-3 font-medium gap-2 cursor-pointer ml-8 mt-5'>
           {pageName}
@@ -15,8 +38,23 @@ const Classnotes = () => {
       <div className="flex justify-center items-center">
         <div className="w-[94%]  h-[1.5px] bg-[#D9D9D9] my-0"></div>
       </div>
-      {pageName === "All Class Notes" && <ClassnotesCard setPageName={setPageName} />}
-      {pageName === "Data Structure" && <ClassnotesTable setPageName={setPageName} />}
+      {pageName === "All Class Notes" && 
+        <div className='w-full grid grid-cols-3 gap-4 ml-5'>
+          {classNotes.length > 0 ? (
+            classNotes.map((note, index) => (
+              <ClassnotesCard 
+                key={index} 
+                setPageName={setPageName} 
+                subjectName={note.subject.name} 
+                setSubjectName={setSubjectName} 
+              />
+            ))
+          ) : (
+            <div className='mt-10 font-medium text-xl text-[#1A1A1A]'>No assignments available.</div>
+          )}
+        </div>
+      }
+      {pageName === subjectName && <ClassnotesTable setPageName={setPageName} />}
     </div>
   );
 }
--- Student_Side/src/components/ClassroomComponents/ClassnotesCard.jsx
+++ Student_Side/src/components/ClassroomComponents/ClassnotesCard.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import profile from '../../assets/classNotesProfile.svg';
 import folder from '../../assets/classNotesFolder.svg';
 
-function ClassnotesCard({ setPageName }) {
+function ClassnotesCard({ setPageName, subjectName, setSubjectName }) {
   const folderStack = () => {
-    setPageName("Data Structure");
+    setSubjectName(subjectName); 
+    setPageName(subjectName); 
   };
 
   return (
     <div className="m-8 flex flex-col justify-center w-[22rem] rounded-[15px] overflow-hidden shadow-lg border border-[#7F7F7F]">
       <div className='bg-[#004BB8]'>
-        <div className='flex w-2/3 font-medium text-2xl text-white ml-7 mt-4'>Data Structure</div>
+        <div className='flex w-2/3 font-medium text-2xl text-white ml-7 mt-4'>{subjectName}</div>
         <div className='ml-7 font-light text-xs text-white'>2023-24</div>
         <div className='flex justify-between'>
           <div className='ml-7 mt-2 mb-0 font-normal text-base text-white'>Swati Tomar </div>
