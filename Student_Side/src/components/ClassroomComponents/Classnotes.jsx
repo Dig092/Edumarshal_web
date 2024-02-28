419150e794b66f4e28aa22ecac4a52d244bbc1f8
abhineet <abhii.neat@gmail.com>
2024-02-29 03:02:02 +0530

    made classroom pages responsive

--- Student_Side/src/components/ClassroomComponents/Classnotes.jsx
+++ Student_Side/src/components/ClassroomComponents/Classnotes.jsx
@@ -29,7 +29,7 @@ const Classnotes = () => {
   };
 
   return (
-    <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-y-auto overflow-x-hidden">
+    <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[1024px]:ml-[80px] max-[1024px]:w-[98%] max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-y-auto overflow-x-hidden">
       <div className="h-[9.6vh] sticky top-0 bg-white mr-4  flex flex-wrap items-center gap-3 text-lg ml-6">
         <div className='flex items-center p-3 font-medium gap-2 cursor-pointer ml-8 mt-5'>
           {pageName}
@@ -39,7 +39,8 @@ const Classnotes = () => {
         <div className="w-[94%]  h-[1.5px] bg-[#D9D9D9] my-0"></div>
       </div>
       {pageName === "All Class Notes" && 
-        <div className='w-full grid grid-cols-3 gap-4 ml-5'>
+      <div className='max-[500px]:flex max-[500px]:justify-center max-[500px]:items-center'>
+        <div className='w-full max-[500px]:w-[70%] grid grid-cols-3 max-[768px]:grid-cols-2 max-[768px]:ml-6 max-[500px]:grid-cols-1  max-[1024px]:grid-cols-3 gap-4 max-[1024px]:gap-0 max-[1024px]:m-0 ml-5'>
           {classNotes.length > 0 ? (
             classNotes.map((note, index) => (
               <ClassnotesCard 
@@ -52,6 +53,7 @@ const Classnotes = () => {
           ) : (
             <div className='mt-10 font-medium text-xl text-[#1A1A1A]'>No assignments available.</div>
           )}
+        </div>
         </div>
       }
       {pageName === subjectName && <ClassnotesTable setPageName={setPageName} />}
