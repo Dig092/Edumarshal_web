d25410a5296cad8b0ce5c0c8965b0bd1a7fcf393
abhineet <abhii.neat@gmail.com>
2024-04-04 16:26:02 +0530

    recent changes

--- Student_Side/src/components/ClassroomComponents/Attendance.jsx
+++ Student_Side/src/components/ClassroomComponents/Attendance.jsx
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
 
-const Attendance = () => {
-  const [activeoption, setActiveoption] = useState(0);
-  const handleoptionClick = (index) => {
-    setActiveoption(index);
-  };
-
-  return (
-    <div className="bg-[#ffffff] h-[72vh] rounded-3xl mx-4 mt-4 overflow-y-auto flex flex-col justify-center items-center">
-        
-      <div className="my-6 w-[95%] sticky bg-white ml-4 flex flex-wrap items-center justify-between">
-        <div>Overall Attendance</div>
-        <div className="flex gap-5 pr-8">
-          <h1>Month</h1>
-          <h1>Week</h1>
-          <h1>Day</h1>
-        </div>
-      </div>
-
-      <div className="w-[94%]  h-[2px] bg-[#D9D9D9] mb-6"></div>
-
-      {activeoption === 0 && (
-        <div className="w-[95%] m-auto flex h-full justify-evenly">
-          <div className="w-[25%] bg-[#F2F6FF] h-full rounded-2xl">
-
-          </div>
-          <div className="w-[70%] bg-[#F2F6FF] h-full rounded-2xl"></div>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Attendance;
-=======
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { CircularProgressbar } from "react-circular-progressbar";
@@ -167,4 +131,4 @@ const Attendance = () => {
 };
 
 export default Attendance;
->>>>>>> 7bbcda4ee61b1ddc1c267f9f274ca17eb139e555
+
--- Student_Side/src/components/ClassroomComponents/Syllabus.jsx
+++ Student_Side/src/components/ClassroomComponents/Syllabus.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Syllabus = () => {
+  const [syllabusPdf, setSyllabusPdf] = useState('');
+
+  useEffect(() => {
+    getTimetable();
+  }, []);
 
   const getTimetable = () => {
     axios
-        .get("https://akgec-edu.onrender.com/v1/student/syllabus", {
-            withCredentials: true,
-        })
-        .then((res) => {
-            const syllabus = res.data.examTimetable.examTimetableUrl;
-            console.log(syllabus);
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-};
-  return (
-    <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[1024px]:ml-[80px] max-[1024px]:w-[98%]  max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-auto"></div>
-  )
-}
+      .get("https://akgec-edu.onrender.com/v1/student/syllabus", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        const syllabus = res.data.syllabus.syllabus;
+        console.log(syllabus);
+        setSyllabusPdf(syllabus);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-export default Syllabus
+  const openPdfInNewTab = () => {
+    window.open(syllabusPdf, '_blank');
+  };
+
+  return (
+    <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[1024px]:ml-[80px] max-[1024px]:w-[98%]  max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-auto">
+      <div className="pdf-card" onClick={openPdfInNewTab}>
+      <object width="20%" height="400" data={syllabusPdf} type="application/pdf">   </object>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Syllabus;
