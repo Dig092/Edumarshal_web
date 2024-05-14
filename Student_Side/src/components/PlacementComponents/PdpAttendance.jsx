4edc35a9b4ece756a8e6acef2580194fe63012bb
abhinav2210103 <abhinav2210103@akgec.ac.in>
2024-05-14 20:16:29 +0530

    added placement in student side

---
+++ Student_Side/src/components/PlacementComponents/PdpAttendance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PdpAttendance() {
+  return (
+    <div>PdpAttendance</div>
+  )
+}
+
+export default PdpAttendance
---
+++ Student_Side/src/components/PlacementComponents/PlacementEvent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PlacementEvent() {
+  return (
+    <div>PlacementEvent</div>
+  )
+}
+
+export default PlacementEvent
