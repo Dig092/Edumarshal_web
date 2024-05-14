25b03165a13c7384e557dd175c1b25176ace3d0c
abhinav2210103 <abhinav2210103@akgec.ac.in>
2024-05-14 20:51:28 +0530

    Input Attachments added

--- Student_Side/src/components/PlacementComponents/Internship.jsx
+++ Student_Side/src/components/PlacementComponents/Internship.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useRef } from 'react';
 const Labels = [
   { name: "Company Name", smallWidth: "w-[16rem]" },
   { name: "Project Title", smallWidth: "w-[16rem]" },
@@ -13,7 +14,18 @@ const Labels = [
   { name: "End Date",  smallWidth: "w-[16rem]" },
 ];
 function Internship() {
+  const fileInputRef = useRef(null);
+  const handleupload = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+  }
+  }
+  const handleselect = (event) => {
+    const selectedFile = event.target.files[0];
+    console.log("Selected file:", selectedFile);
+  }
   return (
+    <>
     <div>
     <div className="h-[3rem] bg-[#004BB8] my-5 rounded-[0.5rem] items-center md:justify-between justify-center md:px-12 px-6 flex cursor-pointer sm:mx-10 ml-2 text-lg text-[#FFFFFF]">
       <pre>Internship Details</pre>
@@ -37,7 +49,21 @@ function Internship() {
         </div>
       ))}
     </div>
-  </div>
+    <div className="flex items-center ml-12 md:gap-5 gap-2 font-medium justify-start flex-wrap">
+            <div>Attachments (5MB MAX)</div>
+            <div className="bg-[#004BB8] h-[2.5rem] w-[3.5rem] flex items-center justify-center rounded-xl cursor-pointer" onClick={handleupload}><img src="./uploadarrow1.svg"/></div>
+          </div>
+    </div>
+    <input
+                type="file"
+                id="fileInput"
+                name="fileInput"
+                accept=".jpg,.jpeg,.png,.heic"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                onChange={handleselect}
+            />
+</>
   )
 }
 
