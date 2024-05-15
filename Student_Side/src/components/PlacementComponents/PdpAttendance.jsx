1180d1df47637c7e6186fa957d2b228dfa076245
abhinav2210103 <abhinav2210103@akgec.ac.in>
2024-05-15 17:44:02 +0530

    sm screen placement responsive

--- Student_Side/src/components/PlacementComponents/PdpAttendance.jsx
+++ Student_Side/src/components/PlacementComponents/PdpAttendance.jsx
@@ -29,10 +29,11 @@ function PdpAttendance() {
       <div className="h-[3rem] bg-[#004BB8] my-5 rounded-[0.5rem] items-center md:justify-between justify-center md:px-12 px-6 flex cursor-pointer sm:mx-10 ml-2 text-lg text-[#FFFFFF]">
         <pre>Attendance Details</pre>
       </div>
+      <div className='flex flex-col justify-center items-center md:items-start'>
       {data.attendance.map((item, index) => (
-        <div className='border-2 h-[7vh] md:w-[28rem] w-[20rem] ml-12 border-black rounded-2xl flex justify-start items-center my-2' key={index}>
+        <div className='border-2 h-[6vh] md:w-[28rem] w-[18rem] md:ml-12 ml-2 border-black rounded-2xl flex items-center my-2' key={index}>
           <div className='flex md:ml-10 gap-10'>
-            <div>{item.date}</div>
+            <div className='font-semibold'>{item.date}</div>
             <div
               className='ml-7'
               style={{ color: item.attended ? 'green' : 'red' }}
@@ -42,6 +43,7 @@ function PdpAttendance() {
           </div>
         </div>
       ))}
+    </div>
     </>
   );
 }
