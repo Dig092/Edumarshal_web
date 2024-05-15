63c3f7c6b7567bba78a7f6dbeb6e556d4fbeb436
abhineet <abhii.neat@gmail.com>
2024-05-15 19:39:34 +0530

    change in env

--- Student_Side/src/components/ClassroomComponents/Syllabus.jsx
+++ Student_Side/src/components/ClassroomComponents/Syllabus.jsx
@@ -2,22 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Syllabus = () => {
-    const getTimetable = () => {
-        axios
+  const getTimetable = () => {
+    axios
             .get(import.meta.env.VITE_BACKEND_API + "/v1/student/syllabus", {
-                withCredentials: true,
-            })
-            .then((res) => {
+        withCredentials: true,
+      })
+      .then((res) => {
                 const syllabus = res.data.examTimetable.examTimetableUrl;
-                console.log(syllabus);
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    };
-    return (
+        console.log(syllabus);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  return (
         <div className="bg-[#ffffff] h-[95%] max-[768px]:h-[100%] max-[1024px]:ml-[80px] max-[1024px]:w-[98%]  max-[768px]:ml-[4.75rem] rounded-3xl mx-4 mt-4 overflow-auto"></div>
-    );
+  );
 };
 
 export default Syllabus;
