96243ce80123908e31e9a1bcc536793e6466431f
abhinav2210103 <abhinav2210103@akgec.ac.in>
2024-05-17 23:57:51 +0530

    get api integerated in classnotes_Teacher

--- Teacher_Side/src/components/Classroom/ClassNotes.jsx
+++ Teacher_Side/src/components/Classroom/ClassNotes.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import { useEffect,useState } from 'react';
+import axios from "axios";
 function ClassNotes() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchClassnotes();
+}, []);
+
+const fetchClassnotes = async () => {
+    try {
+        const response = await axios.get(
+          "https://akgec-edu.onrender.com/v1/student/classNotes",
+            {
+                withCredentials: true,
+            }
+        );
+        setData(response.data.classNotes);
+        console.log(response.data.classNotes[0].classNotes);
+    } catch (error) {
+        console.error("Error fetching documents:", error);
+    }
+};
   return (
-    <div>ClassNotes</div>
+  <div>
+    classnotes
+  
+  </div>
   )
 }
 
