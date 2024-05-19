9728aa9ad7529a039c154bc0cffb15faf339d4c1
Dhruval Gupta <diggupta092@gmail.com>
2024-05-19 08:23:33 +0530

    Added Skeleton Loader

--- Student_Side/tailwind.config.js
+++ Student_Side/tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       screens: {
         'xl': '1440px',    
         'lg': '1024px',    
-        'md': '768px',
+        'md': '769px',
         'sm': '400px',
         'xmd':'900px'
       },
