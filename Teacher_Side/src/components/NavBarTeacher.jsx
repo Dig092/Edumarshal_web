d409f6a4ca6117714babb4f926fd7fbd516e9a91
Akshar Saxena <aksharsaxena2003@gmail.com>
2024-06-01 19:55:00 +0530

    partially completed dashboard teacher side

--- Teacher_Side/src/components/NavBarTeacher.jsx
+++ Teacher_Side/src/components/NavBarTeacher.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NavBarTeacher(props) {
     return (
-        <div className='bg-white px-10 py-8 w-full mx-auto h-[60px] flex justify-between items-center'>
+        <div className='bg-white ml-16 px-10 py-8 w-full mx-auto h-[60px] flex justify-between items-center'>
             <h1 className='text-xl text-black font-medium'>{props.title}</h1>
             <div className='flex w-[300px] justify-between items-center'>
                 <img className='w-[25px] h-[20px]' src="./icons/email.png" alt="" />
